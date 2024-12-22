1eb59ef72df34e1124e8706bbc3e1c477c47f526
tanushree1810 <tkaushik901@gmail.com>
2024-12-22 10:58:47 +0530

    changes in userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-import userModel from "../models/userModel.js";
+import userModel from "../models/UserModel.js";
 import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken';
 import validator from "validator";
